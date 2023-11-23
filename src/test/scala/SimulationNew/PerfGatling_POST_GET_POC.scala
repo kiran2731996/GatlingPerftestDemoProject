f4331aa940d4a0ef5c481504029c3cfa0d35bee1
Kiran <E005271@cigniti.com>
2023-11-23 21:04:34 +0530

    Gatling_Githubcommit

--- src/test/scala/SimulationNew/PerfGatling_POST_GET_POC.scala
+++ src/test/scala/SimulationNew/PerfGatling_POST_GET_POC.scala
@@ -1,8 +1,9 @@
-package Com.aso.perftest
+package SimulationNew
 
 import io.gatling.core.Predef._
-import io.gatling.http.Predef._
 import io.gatling.core.structure.ChainBuilder
+import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
 
 class PerfGatling_POST_GET_POC extends Simulation
@@ -20,15 +21,23 @@ class PerfGatling_POST_GET_POC extends Simulation
           .header(name = "content-type", value = "application/json")
           .asJson
           .body(RawFileBody("data/user.json")).asJson
+          .check( bodyString.saveAs( "RESPONSE_DATA" ) )
           .check(
-            status is 200
+            status is 200,
+            substring("profile")
+
           )
 
       )
+      .exec(session => {
+        println("Some Restful Service Response Body:")
+        println(session("RESPONSE_DATA").as[String])
+        session
+      })
     }
 
   def Minicart() : ChainBuilder= {
-    repeat(5) {
+    repeat(1) {
       exec(
         http(requestName = "Minicart.PG")
           .get("/api/cart/000000/summary")
@@ -48,5 +57,5 @@ class PerfGatling_POST_GET_POC extends Simulation
 
 
 //  setUp(sc1.inject(atOnceUsers(users = 1))).protocols(httpProtocolconfig)
-  setUp(sc1.inject(rampUsers(5).during(1.minutes))).maxDuration(5.minutes).protocols(httpProtocolconfig)
+  setUp(sc1.inject(rampUsers(2).during(1.minutes))).maxDuration(5.minutes).protocols(httpProtocolconfig)
 }
